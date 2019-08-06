a4afabfc553333bf61699f7ca07b2af92774875d
da5nsy <dannygarside@outlook.com>
2019-08-06 14:04:20 +0100

    add upvp

--- PTBColorimetryDemo.m
+++ PTBColorimetryDemo.m
@@ -48,7 +48,7 @@ clc, clear, close all
 % Illuminant - aka Spectral Power Distribution (SPD)
 % In this example we use D65 which is considered a standard daylight 
 % (https://en.wikipedia.org/wiki/Illuminant_D65)
-% For explanation of PTB colorimetric data standards see PsychColorimetricData/PsychColorimetricMatFiles/Contents.m
+% For explanation of PTB colorimetric data format standards see PsychColorimetricData/PsychColorimetricMatFiles/Contents.m
 load spd_D65
 
 % Surface - aka Surface Reflectance Function (SRF)
@@ -63,9 +63,20 @@ load T_xyz1931
 
 % These are just examples of many options, see PsychColorimetricData/PsychColorimetricMatFiles/Contents.m
 
+%% Plot data
+
+figure,
+plot(SToWls(S_D65),spd_D65)
+
+figure,
+plot(SToWls(S_vrhel),sur_vrhel)
+
+figure,
+plot(SToWls(S_xyz1931),T_xyz1931)
+
 %% Interpolate values to match
 % Both S_D65 and S_xyz1931 have range/intervals of [380,5,81], so we'll
-% choose to modify sur_vrhel (and S_vrhel)
+% choose to downsample/interpolate sur_vrhel (and S_vrhel)
 
 sur_vrhel = SplineSrf(S_vrhel,sur_vrhel,S_D65,1);
 S_vrhel = S_D65;
@@ -73,27 +84,59 @@ S_vrhel = S_D65;
 figure, plot(SToWls(S_vrhel), sur_vrhel)
 xlabel('Wavelength (nm)')
 ylabel('Reflectance')
-% Note how the flag at the end of the SplineSrf call has requested that
-% extrapolation be done by extending with final values.
+% (Note how the flag at the end of the SplineSrf call has requested that
+% extrapolation be done by extending with final values.)
 
 %% Compute colorimetry
 
-colourSignals = sur_vrhel.*spd_D65;
-XYZ = T_xyz1931*colourSignals;
-xy = [XYZ(1,:)./sum(XYZ);XYZ(2,:)./sum(XYZ)]; % Or use XYZToxyY.m
+colourSignals = sur_vrhel.*spd_D65; % This is nominally the light actually reaching the eye
+
+XYZ = T_xyz1931*colourSignals; % These are 'tristimulus values'
+
+xy = [XYZ(1,:)./sum(XYZ);XYZ(2,:)./sum(XYZ)]; % 'Chromaticity co-ordinates' (Alternative: use XYZToxyY.m)
 
 %% Plot colorimetry
 
-spectralLocus = [T_xyz1931(1,:)./sum(T_xyz1931);T_xyz1931(2,:)./sum(T_xyz1931)];
+spectralLocus_xy = [T_xyz1931(1,:)./sum(T_xyz1931);T_xyz1931(2,:)./sum(T_xyz1931)];
 sRGBSpectralLocus = XYZToSRGBPrimary(T_xyz1931); %These values go considerably out of gamut, but we only want rough values to orient ourselves
 
 figure, hold on, 
 axis equal
 axis([0 1 0 1])
-scatter(spectralLocus(1,:),spectralLocus(2,:),[],sRGBSpectralLocus','filled')
+scatter(spectralLocus_xy(1,:),spectralLocus_xy(2,:),[],sRGBSpectralLocus','filled')
 scatter(xy(1,:),xy(2,:),'kv')
 xlabel('x')
 ylabel('y')
+
+%% Calculate u'v' chromaticity
+
+% More perceptually uniform than xy
+
+upvp = xyTouv(xy); %upvp is short for u prime v prime, because there is another colourspace called uv which is subtly different *sigh*
+sRGBSpectralLocus_upvp = xyTouv(spectralLocus_xy);
+
+% manually from XYZ:
+upvp_manFromXYZ = [4*XYZ(1,:)./(XYZ(1,:) + 15*XYZ(2,:) + 3*XYZ(3,:));...
+    9*XYZ(2,:)./(XYZ(1,:) + 15*XYZ(2,:) + 3*XYZ(3,:))];
+    
+% manually from xy:
+upvp_manFromxy = [4*xy(1,:)./(-2*xy(1,:) + 12*xy(2,:) + 3);...
+    9*xy(2,:)./(-2*xy(1,:) + 12*xy(2,:) + 3)];
+
+% check:
+% find the maximum absolute difference between the above different methods
+max(max(abs(upvp-upvp_manFromxy)))
+max(max(abs(upvp-upvp_manFromXYZ)))
+max(max(abs(upvp_manFromxy-upvp_manFromXYZ))) 
+% should be tiny numbers (e.g. 2e-16), probably rounding errors
+
+figure, hold on, 
+axis equal
+axis([0 1 0 1])
+scatter(sRGBSpectralLocus_upvp(1,:),sRGBSpectralLocus_upvp(2,:),[],sRGBSpectralLocus','filled')
+scatter(upvp(1,:),upvp(2,:),'kv')
+xlabel('u''') %to get an apostrophe you throw extra apostrophes at it until it behaves
+ylabel('v''')
 
 %% - %%
 
@@ -106,7 +149,7 @@ whiteXYZ = T_xyz1931*spd_D65;
 Lab = XYZToLab(XYZ,whiteXYZ);
 
 figure, 
-scatter3(Lab(2,:),Lab(3,:),Lab(1,:)) %To plot L on the Z axis
+scatter3(Lab(2,:),Lab(3,:),Lab(1,:)) 
 
 %% Calculate the Correlated Colour Temperature of an illuminant
 SPDToCCT(spd_D65,S_D65)
